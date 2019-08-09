081fb21faf8cc902d5ca339d3121c4eabf0af337
Pablo Inigo <pabloinigo@gmail.com>
2019-08-09 17:24:27 +0200

    added packer and terraform

---
+++ packer/baseInitsq.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Updating the system
+sudo yum update -y
+
+#Java to version 8
+sudo yum install -y git java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel-1.8.0.201.b09-0.43.amzn1.x86_64 java-1.8.0-openjdk-src-1.8.0.201.b09-0.43.amzn1.x86_64 docker
+sudo alternatives --remove java /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/java
+sudo alternatives --remove javac /usr/lib/jvm/jre-1.7.0-openjdk.x86_64/bin/javac
+sudo alternatives --install /usr/bin/java java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java 1
+sudo alternatives --install /usr/bin/javac javac /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/javac 1
+
+#installing docker composo to start the application at the end
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo service docker start
+
+#Installing SonarQube
+sudo docker-compose -f /tmp/sonar.yml up -d
---
+++ terraform/jenkins_docker.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "jenkins" {
+  ami           = "ami-0d078af680e7a9b11"
+  instance_type = "t2.medium"
+  availability_zone = "eu-central-1b"
+  key_name = "emirates"
+  vpc_security_group_ids = [
+    "sg-0b62183b0bec6769b"
+  ] 
+  tags = {
+    Name = "Jenkins"
+  }
+}
+resource "aws_instance" "SonarQube" {
+  ami           = "ami-0d6d1720670c85115"
+  instance_type = "t2.medium"
+  availability_zone = "eu-central-1b"
+  key_name = "emirates"
+  vpc_security_group_ids = [
+    "sg-0b62183b0bec6769b"
+  ] 
+  tags = {
+    Name = "SonarQube"
+  }
+}
